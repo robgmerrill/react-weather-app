0c1f50f4d8fa335b73c72ec35df00547839effec
robgmerrill <robgmerrill@gmail.com>
2017-09-06 18:23:07 -0700

    project done

--- src/App.js
+++ src/App.js
@@ -1,41 +1,93 @@
 import React, { Component } from 'react';
+// import "bootstrap/dist/css/bootstrap.css";
+import "bootswatch/journal/bootstrap.css";
+import { Navbar, NavItem, Nav, Grid, Row, Col } from "react-bootstrap";
 
 const places = [
   { name: "Palo Alto", zip: "94308" },
   { name: "Stockton", zip: "95209" },
   { name: "Lodi", zip: "95242" },
-  { name: "Seattle", zip: "94119" }
+  { name: "Seattle", zip: "98119" }
 ]
 
 // create new Componentclass WeatherDisplay
-function WeatherDisplay(props) {
-  return(
-    <h1>Hello, {props.zip}</h1>
-  );
+class WeatherDisplay extends Component {
+  constructor() {
+    super();
+    this.state = {
+      weatherData: null
+    };
+  }
+  componentDidMount() {
+    const zip = this.props.zip;
+    const URL = "http://api.openweathermap.org/data/2.5/weather?q=" +
+      zip +
+      "&appid=b1b35bba8b434a28a0be2a3e1071ae5b&units=imperial";
+    fetch(URL).then(res => res.json()).then(json => {
+      this.setState({ weatherData: json });
+    });
+  }
+  render() {
+    const weatherData = this.state.weatherData;
+    if (!weatherData) return <div>Loading</div>;
+    const weather = weatherData.weather[0];
+    const iconUrl = "http://openweathermap.org/img/w/" + weather.icon + ".png";
+    return (
+      <div>
+        <h1>
+          {weather.main} in {weatherData.name}
+          <img src={iconUrl} alt={weatherData.description} />
+        </h1>
+        <p>Current: {weatherData.main.temp}°</p>
+        <p>High: {weatherData.main.temp_max}°</p>
+        <p>Low: {weatherData.main.temp_min}°</p>
+        <p>Wind Speed: {weatherData.wind.speed} mi/hr</p>
+      </div>
+    );
+  }
 }
 
+
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      activePlace: 0,
+    }
+  }
   render() {
+    const activePlace = this.state.activePlace;
     return (
-      <div className="App">
-        {/* use map method to go through our places array - do an example in the console */}
-        {/* second field in map allows you to take advantage of index...when ordering is important this isn't the means that you will want to use */}
-        {places.map(function(place, index) {
-          {/* the return statement is what this component is going to render to the DOM */}
-          return (
-            // {/* perhaps I should do this once without the key prop? - I think so */}
-            <button key={index} onClick={function() {
-              // {/* let's return to the console to make sure that when we click on button we are getting the right index. */}
-                return console.log("Clicked index " + index);
-              }}
-              // these are all the attributes for the opening of our button tag.
+      <div>
+        <Navbar>
+          <Navbar.Header>
+            <Navbar.Brand>
+              React Simple Weather App
+            </Navbar.Brand>
+          </Navbar.Header>
+        </Navbar>
+        <Grid>
+          <Row>
+            <Col md={4} sm={4}>
+              <h3>Select a city</h3>
+              <Nav
+                bsStyle="pills"
+                stacked
+                activeKey={activePlace}
+                onSelect={index => {
+                  this.setState({ activePlace: index });
+                }}
               >
-              {/* }// name the button with place and name. Our first time through will be "Palo Alto" */}
-              {place.name}
-              {/* }// let's close out the button */}
-              </button>
-          )
-        })}
+                {places.map((place, index) => (
+                  <NavItem key={index} eventKey={index}>{place.name}</NavItem>
+                ))}
+              </Nav>
+            </Col>
+            <Col md={8} sm={8}>
+              <WeatherDisplay key={activePlace} zip={places[activePlace].zip} />
+            </Col>
+          </Row>
+        </Grid>
       </div>
     );
   }
